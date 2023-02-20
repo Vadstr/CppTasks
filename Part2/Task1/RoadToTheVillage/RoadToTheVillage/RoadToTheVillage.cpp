37bfc56dd2a9bb74b8db264e7aa0cce4dff7c495
Striletskyi Vadym <StriletskiyVadim@gmail.com>
2023-02-20 22:12:44 +0200

    Upload tasks to part 2 Part 2 Task 1 Road to the village done

---
+++ Part2/Task1/RoadToTheVillage/RoadToTheVillage/RoadToTheVillage.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+float roadCoast(float, float, float);
+
+int main()
+{
+    cout << "Input distance to the village (in km): ";
+    float distance;
+    cin >> distance;
+
+    cout << "Input fuel cost (per litr): ";
+    float fuelCost;
+    cin >> fuelCost;
+
+    cout << "Input fuel consumption (litr/100km): ";
+    float fuelConsumption;
+    cin >> fuelConsumption;
+
+    float oneWayCost = roadCoast(distance, fuelCost, fuelConsumption);
+
+    cout << "The road to the village and back costs " << oneWayCost*2;
+}
+
+float roadCoast(float distance, float fuelCost, float fuelConsumption)
+{
+    return distance * (fuelConsumption / 100) * fuelCost;
+}
+
