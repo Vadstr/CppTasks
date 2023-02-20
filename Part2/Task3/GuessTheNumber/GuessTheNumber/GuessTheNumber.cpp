b67da882fc83aa86674f678e2cacff0c8ace17b9
Striletskyi Vadym <StriletskiyVadim@gmail.com>
2023-02-20 23:22:16 +0200

    Part 2 Task 3 Guess the number done

---
+++ Part2/Task3/GuessTheNumber/GuessTheNumber/GuessTheNumber.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+int getRandomNumber(int,int);
+
+int main()
+{
+    int minValue = 100, maxValue = 999;
+    int num, wrongPlace, correct;
+    num = getRandomNumber(minValue, maxValue);
+    string randNum = to_string(num);
+    cout << "Try to guess the number" << endl;
+    while (true)
+    {
+        wrongPlace = 0;
+        correct = 0;
+        cout << "Your variant: ";
+        string guessNum;
+        cin >> guessNum;
+        if (guessNum.length() != 3)
+        {
+            continue;
+        }
+
+        for (int i = 0; i < guessNum.length(); i++)
+            for (int j = 0; j < randNum.length(); j++)
+                if (guessNum[i] == randNum[j])
+                    if (i == j)
+                        correct++;
+                    else
+                        wrongPlace++;
+        
+        if (correct == 3)
+            break;
+
+        cout << "Guessed: " << (wrongPlace + correct) << "\tOn right place: " << correct << endl;
+    }
+    cout << "Congratulation! It`s correct";
+}
+
+int getRandomNumber(int min, int max)
+{
+    srand(time(NULL));
+    int num = min + rand() % (max - min + 1);
+
+    return num;
+}
