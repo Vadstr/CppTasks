a1ef67bbc2b92614a45bfc05a214f7225eb09e57
Striletskyi Vadym <StriletskiyVadim@gmail.com>
2023-02-20 20:23:07 +0200

    Part 1 Task 3 What season done

---
+++ Part1/Task3/WhatSeason/WhatSeason/WhatSeason.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int askNumber();
+void whatTheSeason(int);
+
+int main()
+{
+    int mounthIndex = askNumber();
+    
+    if (mounthIndex > 0 && mounthIndex < 13)
+        whatTheSeason(mounthIndex);
+    else
+        cout << "Incorect mounth index";
+}
+
+int askNumber() 
+{
+    cout << "Input the number of mounth:";
+    int number;
+    cin >> number;
+    return number;
+}
+
+void whatTheSeason(int mounthIndex) 
+{
+    string season;
+    if (mounthIndex < 3 || mounthIndex > 11)
+        season = "winter";
+    else if (mounthIndex < 6)
+        season = "spring";
+    else if (mounthIndex < 9)
+        season = "summer";
+    else
+        season = "autumn";
+
+    cout << season;
+}
