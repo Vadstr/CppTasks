b0f103a2584251423cc730368af05266719be305
Striletskyi Vadym <StriletskiyVadim@gmail.com>
2023-02-20 15:37:00 +0200

    Part 1 Task 2 The smallest number done

---
+++ Part1/Task2/TheSmallestNumber/TheSmallestNumber/TheSmallestNumber.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int askNumber(int);
+
+int main()
+{
+    cout << "Input count of number: ";
+    int countOfNumber, newNum, smallestNumber;
+    cin >> countOfNumber;
+
+    smallestNumber = INT32_MAX;
+
+    for (int i = 0; i < countOfNumber; i++)
+    {
+        newNum = askNumber(i + 1);
+        if (newNum < 0)
+        {
+            cout << "Pleas don`t write negative numbers" << endl << "Try again!" << endl;
+            i--;
+        }
+        else if (newNum < smallestNumber)
+            smallestNumber = newNum;
+    }
+
+    cout << "The smallest number is " << smallestNumber;
+}
+
+int askNumber(int index) 
+{
+    cout << "Input number" << index << ": ";
+    int num;
+    cin >> num;
+    return num;
+}
