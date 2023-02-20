c00d326b29b3c43b98e3cc7f8d840fa333099ac3
Striletskyi Vadym <StriletskiyVadim@gmail.com>
2023-02-20 15:14:28 +0200

    Part 1 Task 1 IcecreamSandwich done

--- Part1/Task1/IcecreamSandwich/IcecreamSandwich/IcecreamSandwich.cpp
+++ Part1/Task1/IcecreamSandwich/IcecreamSandwich/IcecreamSandwich.cpp
@@ -1,6 +1,35 @@
 ﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isIcecreamSandwich(string);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string sandwich;
+    cout << "Write some word: ";
+    cin >> sandwich;
+    if (isIcecreamSandwich(sandwich))
+        cout << sandwich << " is Icecream Sandwich";
+    else
+        cout << sandwich << " is NOT Icecream Sandwich";
+}
+
+bool isIcecreamSandwich(string sandwich)
+{
+    int sandwichLenght = sandwich.length();
+    if (sandwichLenght < 3)
+        return false;
+
+    if (sandwich[0] == sandwich[sandwichLenght / 2])
+        return false;
+
+    for (int i = 0; i < sandwichLenght/2; i++)
+    {
+        if (sandwich[i] != sandwich[sandwichLenght - i - 1])
+            return false;
+    }
+
+    return true;
 }
