ab72e0fbce30c4d860fb383ff2a00c93297d099c
Striletskyi Vadym <StriletskiyVadim@gmail.com>
2023-02-21 13:36:09 +0200

    Part 3 Task 2 Sum of even number done

---
+++ Part3/Task2/SumOfEvenNumbers/SumOfEvenNumbers/SumOfEvenNumbers.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void calculate(int);
+
+int main()
+{
+    cout << "Input count of even number:";
+    int countOfNum;
+    cin >> countOfNum;
+
+    calculate(countOfNum);
+}
+
+void calculate(int count) 
+{
+    int sum = 0;
+    for (int i = 1; i <= count; i++)
+    {
+        sum += i * 2;
+        cout << i * 2;
+        if (i != count)
+            cout << " + ";
+    }
+
+    cout << " = " << sum << endl;
+}
