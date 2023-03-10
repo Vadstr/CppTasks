fc93480b3ae59911ad4e2cff97cf6dc4627b9d8a
Striletskyi Vadym <StriletskiyVadim@gmail.com>
2023-03-10 11:00:48 +0200

    Part 4 Task 1 verst to Km done

---
+++ Part4/Task1/VerstToMeter/VerstToMeter/VerstToMeter.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+float _kmInVerst = 1.0668;
+
+float verstToKm(float);
+float inputDistance();
+
+int main()
+{
+	cout << "In Km it`s: " << verstToKm(inputDistance()) << endl;
+}
+
+float verstToKm(float distance) 
+{
+	return distance * _kmInVerst;
+}
+
+float inputDistance() 
+{
+    float distance;
+
+	cout << "Input distance in verst: ";
+	cin >> distance;
+
+	return distance;
+}
